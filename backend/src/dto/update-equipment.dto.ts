1178ddaccf7b6e80fa0b283b4c201e99b3e22389
Chiku934 <your-email@example.com>
2026-01-16 15:18:48 +0530

    Fix ESLint warning in update-equipment.dto.ts

--- backend/src/dto/update-equipment.dto.ts
+++ backend/src/dto/update-equipment.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEquipmentDto } from './create-equipment.dto';
 
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 export class UpdateEquipmentDto extends PartialType(CreateEquipmentDto) {}
